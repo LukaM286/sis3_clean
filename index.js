50579084d4ff9660d27b98ecbcc5016523b2fdc7
Luka Mišmaš <mismasluka@gmail.com>
2025-07-27 12:13:30 +0200

    Changes, fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import dependencies and initialize the express app
+
 const express = require('express')
 require('dotenv').config()
 const app = express()
@@ -6,49 +6,40 @@ const cors = require("cors")
 const path = require('path')
 const port = process.env.PORT || 7555
 
-// import local files
-const novice = require('./routes/novice')
+
+const novice = require('./routes/novice') 
 const users = require('./routes/users')
 const upload = require('./routes/upload')
 
-
 const session = require('express-session')
 
-
-app.set('trust proxy', 1) // trust first proxy
+app.set('trust proxy', 1) 
 app.use(session({
    secret: 'some secret',
    resave: true,
    saveUninitialized: true,
    cookie: { secure: false }
-  }))
-
-
-//Some configurations
-app.use(express.urlencoded({extended : true}));
-app.use(cors({
-   origin: 'http://localhost:3000',
-   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
-   credentials: true
 }))
 
-
-
-
-// configurations
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  methods: ["GET", "POST"],
-}))
+app.use(express.urlencoded({extended : true}));
 
-// actual routes
+
+app.use(cors({
+   origin: 'http://localhost:8080',
+   methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'HEAD'],
+   credentials: true
+}));
+
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
-
-
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
 
 app.use('/novice', novice)
 app.use('/users', users)
@@ -56,5 +47,4 @@ app.use('/uploadFile', upload)
 app.use('/delete', novice)
 app.use('/list', users)
 
-// start the express server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
